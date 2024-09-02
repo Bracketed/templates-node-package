963883c4d6b584534270bd0899f4831dd261a588
ninjaninja140 <edenkneale@outlook.com>
2024-09-02 20:11:07 +0100

    Post

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,11 +1,1 @@
-import { Logger, LogLevel } from '../index';
-
-const console = new Logger();
-
-console.info('Hello World!');
-console.debug('Hello World!');
-console.warn('Hello World!');
-console.error('Hello World!');
-console.fatal('Hello World!');
-console.trace('Hello World!');
-console.write(LogLevel.Info, 'Hello World!');
+// Testing things in here
